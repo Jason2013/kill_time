d3bb8b336bed9e0e04bb3bd08244e95192ba0cb9
Jason Chen <shchenchang@gmail.com>
2016-12-03 09:58:48 +0800

    添加 stdafx 头文件

---
+++ src/stdafx.h
@@ -0,0 +1,25 @@
+#ifndef _STDAFX_H_
+#define _STDAFX_H_
+
+#include <windows.h>
+#include <tchar.h>
+#include <io.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <gl/gl.h>				// standard OpenGL include
+#include <gl/glu.h>				// OpenGL utilties
+
+#include <dinput.h>
+#include <dmusicc.h>               // DirectMusic includes
+#include <dmusici.h>
+#include <d3d8types.h>             // for D3DVECTOR
+#include <cguid.h>                 // for GUID_NULL
+
+#include <typeinfo.h>			// for RTTI
+
+#include "vtrace.h"
+
+#endif // _STDAFX_H_
